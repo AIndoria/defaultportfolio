5fa23a257005cd8fc32c14a7eec0eab206c881c5
Abhishek Indoria <indoriaabhishek@gmail.com>
2020-06-02 09:16:57 -0700

    Updates

---
+++ precache-manifest.a00023b14da5c02889a55aec7ef455bd.js
@@ -0,0 +1,50 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "1d673e412a6414cf03f854ee66fecd67",
+    "url": "/defaultportfolio/index.html"
+  },
+  {
+    "revision": "5bcdd6e88a152d93221c",
+    "url": "/defaultportfolio/static/css/main.1dfbe673.chunk.css"
+  },
+  {
+    "revision": "4895b2beda8f62ed097a",
+    "url": "/defaultportfolio/static/js/2.28333d1e.chunk.js"
+  },
+  {
+    "revision": "0749163b59fbee32225059cb60c18af6",
+    "url": "/defaultportfolio/static/js/2.28333d1e.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "5bcdd6e88a152d93221c",
+    "url": "/defaultportfolio/static/js/main.a34a3ab8.chunk.js"
+  },
+  {
+    "revision": "c706f1ba047f72da4356",
+    "url": "/defaultportfolio/static/js/runtime-main.867efaf2.js"
+  },
+  {
+    "revision": "f3e7ab4dc3c22e62e1c553ef9f6742ea",
+    "url": "/defaultportfolio/static/media/Indoria.f3e7ab4d.png"
+  },
+  {
+    "revision": "ae087b78ebf3aaffbe2cc27044555a7e",
+    "url": "/defaultportfolio/static/media/autonomous.ae087b78.png"
+  },
+  {
+    "revision": "a70aec419afa224ef3d0a8aeac63ec93",
+    "url": "/defaultportfolio/static/media/felicity.a70aec41.png"
+  },
+  {
+    "revision": "588cb5abccebcebe8a3265024ae2300f",
+    "url": "/defaultportfolio/static/media/iss-locator.588cb5ab.png"
+  },
+  {
+    "revision": "2481ee019e8afd130b0c9d51dc9da0a3",
+    "url": "/defaultportfolio/static/media/romana.2481ee01.png"
+  },
+  {
+    "revision": "7a8185c04e4db6d0cb4c081ddad8906e",
+    "url": "/defaultportfolio/static/media/wiki.7a8185c0.png"
+  }
+]);
